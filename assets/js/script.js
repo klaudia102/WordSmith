477ca4b90d5b2868042dd03b54107babd62061e5
obeeyoma <147186890+obeeyoma@users.noreply.github.com>
2023-12-21 21:15:17 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var searchBtn = document.querySelector("#searchButton");
 
 // Listen for click events on searchBtn
 // Make API when searchBtn is clicked
-searchBtn.addEventListener("click", getDefinition)
+searchBtn.addEventListener("click", getDefinition);
 
 
 // Function to fetch data
@@ -22,7 +22,7 @@ fetch(queryURL)
 	}).then(function (data) { 
     
 	// Display word definition in console to test
-	console.log(data[0].meanings[0].definitions[0].definition)
+	console.log(data[0].meanings[0].definitions[0].definition);
 		
 	   // Display word definition in textarea  	
         wordMeaningBox.value = data[0].meanings[0].definitions[0].definition;
