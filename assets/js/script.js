0fc53b28c50cce0da47b3f2329f78d72e58a689c
obeeyoma <147186890+obeeyoma@users.noreply.github.com>
2023-12-21 20:14:32 +0100

    Log data to console

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,10 @@ fetch(queryURL)
 		return response.json();
 	}).then(function (data) { 
     
-        // Display word definition in textarea
+	// Display word definition in console to test
+	console.log(data[0].meanings[0].definitions[0].definition)
+		
+	   // Display word definition in textarea  	
         wordMeaningBox.value = data[0].meanings[0].definitions[0].definition;
         
     })
