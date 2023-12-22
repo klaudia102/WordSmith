131b0ba790aaeec5243f2fa298e823186609c347
Robyn <127926809+Robyn-web@users.noreply.github.com>
2023-12-22 08:26:48 +0000

    removed empty wordMeaningBox.textContent = ''

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,24 +7,22 @@ var searchBtn = document.querySelector('#searchButton');
 // Make API when searchBtn is clicked
 searchBtn.addEventListener('click', getDefinition);
 
-
 // Function to fetch data
 function getDefinition() {
-// Get user input
-var word = input.value;
-// Remove whitespace from input string
-word.trim();
-var queryURL = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
+  // Get user input
+  var word = input.value;
+  // Remove whitespace from input string
+  word.trim();
+  var queryURL = 'https://api.dictionaryapi.dev/api/v2/entries/en/' + word;
 
-fetch(queryURL)
-	.then(function (response) {
-		return response.json();
-	}).then(function (data) { 
-    		
-	// Display word definition in div
-	wordMeaningBox.textContent = "";
-        wordMeaningBox.textContent = data[0].meanings[0].definitions[0].definition;
-        
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
     })
-    
+    .then(function (data) {
+      // Display word definition in div
+      //   wordMeaningBox.textContent = '';
+      wordMeaningBox.textContent =
+        data[0].meanings[0].definitions[0].definition;
+    });
 }
