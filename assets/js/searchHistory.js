4ee2a378e06ce8a5fa6c7372dc5064d01b2d8423
obeeyoma <147186890+obeeyoma@users.noreply.github.com>
2024-01-02 15:05:52 +0100

    Show saved searches in a list

--- assets/js/searchHistory.js
+++ assets/js/searchHistory.js
@@ -1,3 +1,15 @@
 // Get references to elements
 var searchHistory = document.querySelector("#search-history");
 var clearBtn = document.querySelector("#clear");
+
+// Variable to store saved searches
+var savedSearches = "";
+
+// Get saved searches from localstorage and add to list
+for (let j = 0; j < localStorage.length; j++) {
+  var savedSearchItem = document.createElement("li");
+  var definitionKey = localStorage.key(j);
+  savedSearchItem.textContent += definitionKey.toUpperCase();
+  savedSearchItem.textContent += " : " + localStorage.getItem(definitionKey);
+  searchHistory.appendChild(savedSearchItem);
+}
